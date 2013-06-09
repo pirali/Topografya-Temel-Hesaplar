0d9f6513b09a5a08862b1b0eaf2c56b3ce8ab573
Özkan ŞEN <ozkansen@gmail.com>
2013-06-09 19:57:36 +0300

    Create ayarlar.py

---
+++ lib/ayarlar.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Programın genel ayarlarının bulunacağı bölüm
+
+
+# Hesap çıktılarının yuvarlanması
+yuvarla = True
+
+# Standart olarak noktadan sonra
+# kaç rakama kadar yuvarlanacak
+_KOORDINAT  = 3
+_ACIDEGERI  = 4
+_MESAFE     = 3
+
+if yuvarla == False:
+    _KOORDINAT, _ACIDEGERI, _MESAFE = 99
